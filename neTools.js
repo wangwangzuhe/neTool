e0d3cdd755817be9a4381deb8c0c44e0f01a3b76
wangwangliu <senla20080808@gmail.com>
2018-12-24 16:56:17 +0800

    Update neTools.js

--- neTools.js
+++ neTools.js
@@ -617,6 +617,39 @@
 		/*埋点*/
 		ca:CA
 	};
+	function isEmptyObj(o) { for (let attr in o) return !1; return !0 }
+	function processArray(arr) {
+	      for (let i = arr.length - 1; i >= 0; i--) {
+		  if (arr[i] === null || arr[i] === undefined) arr.splice(i, 1);
+		  else if (typeof arr[i] == 'object') removeNullItem(arr[i], arr, i);
+	      }
+	      return arr.length == 0
+	  }
+	  function proccessObject(o) {
+	      for (let attr in o) {
+		  if (o[attr] === null || o[attr] === undefined) delete o[attr];
+		  else if(typeof o[attr]=='object') {
+		      removeNullItem(o[attr]);
+		      if (isEmptyObj(o[attr])) delete o[attr];
+		  }
+	      }
+	  }
+	  /**
+	 * 去除对象内属性undefined 
+	 *提交param 时用来判空操作
+	 */
+	  export function removeNullItem(o,arr,i) {
+	      let s = ({}).toString.call(o);
+	      if (s == '[object Array]') {
+		  if (processArray(o) === true) {//o也是数组，并且删除完子项，从所属数组中删除
+		      if (arr) arr.splice(i, 1);
+		  }
+	      }
+	      else if (s == '[object Object]') {
+		  proccessObject(o);
+		  if (arr&&isEmptyObj(o)) arr.splice(i, 1);
+	      }
+	  }
 
 	return neTools;
 });
